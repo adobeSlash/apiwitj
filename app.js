a284f73bf12e1a720355ecb0a927580fb838ae14
KARAGULMEZ Stéphane <stephane.karagulmez@wavestone.com>
2017-05-22 23:40:27 +0200

    ajout d'une version light

--- app.js
+++ app.js
@@ -26,6 +26,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/datatables', express.static(__dirname + '/node_modules/datatables/media/js/'));
+app.use('/datatables/css', express.static(__dirname + '/node_modules/datatables/media/css/'));
+app.use('/datatables/images', express.static(__dirname + '/node_modules/datatables/media/images/'));
 
 app.use('/', index);
 
--- routes/index.js
+++ routes/index.js
@@ -76,4 +76,74 @@ router.get('/', function(req, res, next) {
 
 });
 
+router.get('/stats', function(req, res, next) {
+  res.render('stats', {
+
+  });
+});
+
+router.get('/light', function(req, res, next) {
+
+  const pages = [0]; //list of page we want to parse
+  const articles = []; //put all the article info in
+
+  console.log('start webscraping');
+
+  var scrap = function(_count, _pages, _articles){
+    console.log('webscraping an other page');
+    var url = "http://www.welcometothejungle.co/stacks?q=&hPP=30&idx=cms_companies_stacks_production&p=";
+    var count = _count;
+    var pages = _pages;
+    var articles = _articles;
+
+    if(count >= pages.length){
+      console.log('nb pages = '+ articles.length);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(articles, null, 3));
+    }else{
+      console.log("scrapping pages:" + count);
+      var page = pages[count];
+      const horseman = new Horseman();
+
+      horseman
+          .on('error', console.error)
+          .open(url + '' + page)
+          .evaluate(function() {
+            try{
+              var articlesData = [];
+              $('article').each(function(index, element){
+                 var article = {};
+                 var stackF = [];
+
+                 $(this).find('div[class=company-stack-category]').each(function(i, elem) {
+                   $(this).children('.company-stack-list').children('.stack-item').each(function(c, elem) {
+                     stackF.push($(this).text().replace(/\s+/g, '')); //one stack element
+                   });
+                 });
+                 article.stacks = stackF;
+
+                 var t = $(this).find('h4[class=company-name]').text().replace(/\s+/g, '').replace(/\d+/g, '');
+                 var tRes = t.substr(0, t.length-4);
+                 article.title = tRes;
+
+                 articlesData.push(article);
+               });
+               return articlesData;
+            }catch (err) {
+                return [err];
+            }
+          })
+          .then(article => {
+            console.log('ending scraping pages');
+            articles.push(article);
+            scrap(++count, pages, articles);
+          })
+          .close();
+    }
+  }
+
+  scrap(0,pages,articles);
+
+});
+
 module.exports = router;
